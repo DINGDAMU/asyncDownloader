733c80c7cb0b51d1e35f161c57ad0e57d3758f3b
DINGDAMU <dingdamu@gmail.com>
2018-04-23 18:39:02 +0200

    add coverage

--- tests/test_addtwonum.py
+++ tests/test_addtwonum.py
@@ -3,11 +3,11 @@
 
 import unittest
 import sys
-from asyncDownloaderMain import addtwonum
+from asyncDownloaderMain.addtwonum import *
 class TestClass(unittest.TestCase):
     """Test case docstring."""
     def test_Sample(self):
-        self.assertEqual(3,addtwonum.sumofnum(2,1))
+        self.assertEqual(3,sumofnum(2,1))
 
 if __name__ == "__main__":
     unittest.main()
